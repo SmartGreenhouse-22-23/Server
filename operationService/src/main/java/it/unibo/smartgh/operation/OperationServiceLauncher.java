9f7e1d013d98a42fdb534e022bb8b679d9eebb05
Elena Yan <elena.yan@studio.unibo.it>
2023-01-06 18:24:13 +0100

    feat(operation): integrate with greenhouse communication service

--- operationService/src/main/java/it/unibo/smartgh/operation/OperationServiceLauncher.java
+++ operationService/src/main/java/it/unibo/smartgh/operation/OperationServiceLauncher.java
@@ -16,7 +16,7 @@ public class OperationServiceLauncher {
 
     private static final String OPERATION_DB_NAME = "operation";
     private static final String OPERATION_COLLECTION_NAME = "operation";
-    private static final int PORT = 8892;
+    private static final int PORT = 8896;
     private static final int MONGODB_PORT = 27017;
     private static final String HOST = "localhost";
     private static final String MONGODB_HOST = "localhost";
@@ -25,7 +25,7 @@ public class OperationServiceLauncher {
         Vertx vertx = Vertx.vertx();
         OperationDatabase database = new OperationDatabaseImpl(OPERATION_DB_NAME, OPERATION_COLLECTION_NAME, MONGODB_HOST, MONGODB_PORT);
         OperationController controller = new OperationControllerImpl(database);
-        OperationAPI model = new OperationModel(controller);
+        OperationAPI model = new OperationModel(controller, vertx);
         vertx.deployVerticle(new OperationService(model, HOST, PORT));
     }
 }
